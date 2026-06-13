c490be1cb7708149061221b263bfd8fcc9e9e9ac
unknown <pawel.owerczuk1@gmail.com>
2026-06-13 23:10:04 +0200

    blackjack- ukonczenie prac

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 #include <ctime>
 #include <windows.h>
 using namespace std;
+
+
+int komputer_karty[10];
+int komputer_numer = 0;
+int suma_komputera = 0;
+
 char tk[3][3] = { 
         {'-','-','-'},
         {'-','-','-'},
         {'-','-','-'}
      };
-int tb[13] = {2,3,4,5,6,7,8,9,10,10,10,11};
+int tb[13] = {2,3,4,5,6,7,8,9,10,10,10,10,11};
 
 void wypisanie_karty_b(string figura_karty, string kolor_karty){
     cout << "+-------+\n";
@@ -36,21 +42,86 @@ int losowa_karta_b(){
         
     }
 
+void ruch_komputera(){
+    suma_komputera = 0;
+    for(int i=0;i<komputer_numer;i++){
+        suma_komputera += komputer_karty[i];
+    }
+    while(suma_komputera < 17){
+        komputer_karty[komputer_numer]=losowa_karta_b();
+        komputer_numer++;
+        
+        suma_komputera = 0;
+        for(int i=0;i<komputer_numer;i++){
+            suma_komputera += komputer_karty[i];
+        }
+    }
+}
+
+string figura_karty(int wartosc){
+    if(wartosc==2) return "2";
+    else if(wartosc==3) return "3";
+    else if(wartosc==4) return "4";
+    else if(wartosc==5) return "5";
+    else if(wartosc==6) return "6";
+    else if(wartosc==7) return "7";
+    else if(wartosc==8) return "8";
+    else if(wartosc==9) return "9";
+    if(wartosc==10){
+        int r= rand()%4;
+        if(r==0) return "10";
+        else if(r==1) return "J";
+        else if(r==2) return "Q";
+        else if(r==3) return "K";
+    }
+    if(wartosc==11) return "A";
+    return "?";
+}
+
+string kolor_karty(){
+    int i= rand()%4;
+    if(i==0) return "♠";
+    else if(i==1) return "♥";
+    else if(i==2) return "♦";
+    else if(i==3) return "♣";
+    return "e";
+    
+}
+
+void wypisz_karty_b(int karty[], int numer_karty){
+   for(int i=0;i<numer_karty;i++){
+        string figura = figura_karty(karty[i]);
+        string kolor = kolor_karty();
+        wypisanie_karty_b(figura, kolor);
+   }
+
+}
 void blackjack(){
+komputer_numer = 0;
+suma_komputera = 0;
+int numer_karty = 0;
 int gracz[10];
 int komputer = 0;
 char wybor;
 
-gracz[0] = losowa_karta_b();
-gracz[1] = losowa_karta_b();
+gracz[numer_karty] = losowa_karta_b();
+numer_karty++;
+gracz[numer_karty] = losowa_karta_b();
+numer_karty++;
 
-while(gracz[0] + gracz[1] < 21){
-    cout << "Twoja suma kart: " << gracz[0] + gracz[1] << endl;
+for(;;){
+    int suma=0;
+    for(int i=0;i<numer_karty;i++){
+        suma += gracz[i];
+    }
+    cout << "Twoja suma kart: " << suma << endl;
+    wypisz_karty_b(gracz, numer_karty);
     cout << "Czy chcesz dobrać kartę? (t/n): ";
     cin >> wybor;
 
     if(wybor == 't' || wybor == 'T'){
-        gracz[2] = losowa_karta_b();
+        gracz[numer_karty] = losowa_karta_b();
+        numer_karty++;
     }
     else if(wybor == 'n' || wybor == 'N'){
         break;
@@ -58,17 +129,50 @@ while(gracz[0] + gracz[1] < 21){
     else{
         cout << "Niepoprawny wybór, spróbuj ponownie.\n";
     }
-}
 
-if(gracz[0] + gracz[1] + gracz[2] > 21){
+    if(suma > 21){
+        cout << "Przegrałeś! Twoja suma kart przekroczyła 21.\n";
+        return;
+    }
+}
+int suma=0;
+for(int i=0;i<numer_karty;i++){
+    suma += gracz[i];
+
+}
+if(suma > 21){
     cout << "Przegrałeś! Twoja suma kart przekroczyła 21.\n";
     return;
+
+
 }
+
+komputer_karty[komputer_numer] = losowa_karta_b();
+komputer_numer++;
+komputer_karty[komputer_numer] = losowa_karta_b();
+komputer_numer++;
+ruch_komputera();
+cout << "Karty komputera:\n";
+wypisz_karty_b(komputer_karty, komputer_numer);
+
+if(suma_komputera > 21){
+    cout << "Wygrałeś! Komputer przekroczył 21.\n";
+    return;
+}
+else if(suma>suma_komputera){
+    cout << "Wygrałeś!\n";
+    return;
+}
+else if(suma<suma_komputera){
+    cout << "Przegrałeś!\n";
+    return;
+}
+else{
+    cout << "Remis!\n";
+    return;
 }
 
-
-    
-
+}
 
 void wypisanie_k(){
     cout << "   1   2   3\n" <<  " +---+---+---+\n";
